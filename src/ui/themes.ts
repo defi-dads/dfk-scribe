1d3413dfcbd701cb0df9a33cd003c6be08cb2737
Thomas E Wilson <dev.thomas.e.wilson@gmail.com>
2021-12-04 10:06:30 -0600

    Adding Linear Gradient to styled component sidebar. UI

--- src/ui/themes.ts
+++ src/ui/themes.ts
@@ -1,11 +1,15 @@
 export const themes = {
   light: {
-    primary: '#fff',
-    text: '#1c1c1c',
-    footerBackground: '#f6f6f6',
-    mainColor: '#fff',
-    mainColor2: 'rgb(251, 62, 134)',
-    mainColor3: 'rgb(44, 2, 77)'
+    colors: {
+      primary: '#fff',
+      text: '#000',
+      // secondary: ''
+      blue: '#1c1c1c',
+      footerBackground: '#f6f6f6',
+      mainColor: '#fff',
+      mainColor2: 'rgb(44, 2, 77)',
+      mainColor3: 'rgb(251, 62, 134)'
+    }
   },
 } as const;
 
